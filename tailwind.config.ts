23bb0d0eaa8bd88055f1714f01b99dbdf69c064f
nake226 <a6211089@yahoo.co.jp>
2024-02-20 12:53:06 +0900

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,30 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#253336",
+        blue: "#007CBE",
+      },
+      fontSize: {
+        h1: "48px",
+        h2: "40px",
+        h3: "32px",
+        sub: "24px",
+        paragraph: "16px",
+        caption: "14px",
+        small: "12px",
+      },
+      lineHeight: {
+        h1: "67px",
+        h2: "60px",
+        h3: "48px",
+        sub: "36px",
+        paragraph: "38px",
+        caption: "30px",
+        small: "18px",
+      },
+    },
     screens: {
       sm: { max: "767px" },
       md: "768px",
