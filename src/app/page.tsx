414bbb86cfdf5ab34363a927d118c454fd4d031f
nake226 <a6211089@yahoo.co.jp>
2024-02-20 13:18:51 +0900

    tmp: test .env expression

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_BASIC_USER);
+  console.log(process.env.NEXT_PUBLIC_BASIC_PASSWORD);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>ほえ</h1>
+    </main>
   );
 }
