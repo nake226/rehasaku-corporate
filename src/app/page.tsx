5cbaa7d42a6824914d6641866db41a713ce723d9
nake226 <a6211089@yahoo.co.jp>
2024-03-09 23:27:20 +0900

    feat: mobile header UI, control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Contact } from "@/components/common/Contact";
 import { Footer } from "@/components/common/Footer";
 import { Sitemap } from "@/components/common/Sitemap";
-import { Header } from "@/components/desktop/Header";
+import { Header } from "@/components/common/Header";
 import { About } from "@/components/top/About";
 import { FirstView } from "@/components/top/FirstView";
 import { Mission } from "@/components/top/Mission";
--- src/components/top/FirstView.tsx
+++ src/components/top/FirstView.tsx
@@ -1,6 +1,6 @@
 export const FirstView = () => {
   return (
-    <div className="bg-top-pc sm:bg-top-sp bg-cover bg-center h-screen sm:px-4 grid place-items-center">
+    <div className="bg-top-pc sm:bg-top-sp bg-cover bg-center h-screen sm:h-[calc(100vh_-_57px)] sm:px-4 grid place-items-center">
       <div className="md:w-full md:max-w-[1520px]">
         <p className="text-h2 leading-h2 sm:text-sub sm:leading-sub font-bold sm:drop-shadow-lg md:text-right md:pr-20">
           <span className="sm:text-white">患者さんが主体的になるような</span>
