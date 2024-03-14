10916a90cef3e7c57b5a7a6b56bc9647e14fdd78
nake226 <a6211089@yahoo.co.jp>
2024-03-15 08:51:54 +0900

    chore: use Mission as common component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Sitemap } from "@/components/common/Sitemap";
 import { Header } from "@/components/common/Header";
 import { About } from "@/components/pages/top/About";
 import { FirstView } from "@/components/pages/top/FirstView";
-import { Mission } from "@/components/pages/top/Mission";
+import { Mission } from "@/components/organisms/Mission";
 import { Recruit } from "@/components/pages/top/Recruit";
 import { Service } from "@/components/pages/top/Service";
 
