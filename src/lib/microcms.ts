dfd1b5a8b95da0cc41c24908731f916faf33a900
nake226 <a6211089@yahoo.co.jp>
2024-03-19 09:44:18 +0900

    fix: introduce microCMS

---
+++ src/lib/microcms.ts
@@ -0,0 +1,37 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSDate,
+} from "microcms-js-sdk";
+
+export type News = {
+  id: string;
+  title: string;
+  summary: string;
+  body: JSX.Element;
+  thumbnail: MicroCMSImage;
+  categories: Array<string>;
+} & MicroCMSDate;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
+
+  return listData;
+};
